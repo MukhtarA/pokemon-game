afdef1e748a397fbd72ca9b60ae5673f3db00f35
MukhtarA <33712120+MukhtarA@users.noreply.github.com>
2021-01-29 02:25:48 +0600

    fix: fix styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,22 @@
 import styles from "./styles.module.css";
+import React from "react";
 
-const Layout = ({ id, title, descr, urlBg, colorBg }) => {
-    const style = {
+const Layout = ({id, title, descr, urlBg, colorBg}) => {
 
-    };
-    return(
-        <section className={styles.root} id={id}>
-            <div className={styles.wrapper} style={{ backgroundColor: colorBg, backgroundImage: `url(${urlBg})`}}>
+    return (
+        <section style={{
+            backgroundColor: colorBg,
+            backgroundImage: `url(${urlBg})`
+        }}
+                 className={styles.root} id={id}>
+            <div className={styles.wrapper}>
                 <article>
                     <div className={styles.title}>
                         <h3>{title}</h3>
                         <span className={styles.separator}/>
                     </div>
                     <div className={styles.des}>
-                        <p>{ descr }</p>
+                        <p>{descr}</p>
                     </div>
                 </article>
             </div>
